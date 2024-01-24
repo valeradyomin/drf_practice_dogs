66b6a95796d6e981fd24b2ec28790c5aad64bbc6
Valera <wahrwolffe@gmail.com>
2024-01-24 05:59:07 +0300

    fix urls

--- app_dogs/urls.py
+++ app_dogs/urls.py
@@ -4,13 +4,13 @@ from rest_framework import routers
 from app_dogs.views.breed import BreedViewSet
 from app_dogs.views.dog import DogListView, DogCreateView, DogDetailView, DogUpdateView, DogDeleteView
 
+router = routers.DefaultRouter()
+router.register(r'breed', BreedViewSet, basename='breeds')
+
 urlpatterns = [
     path('', DogListView.as_view(), name='dog_list'),
     path('create/', DogCreateView.as_view(), name='dog_create'),
     path('detail/<int:pk>/', DogDetailView.as_view(), name='dog_detail'),
     path('update/<int:pk>/', DogUpdateView.as_view(), name='dog_update'),
     path('delete/<int:pk>/', DogDeleteView.as_view(), name='dog_delete'),
-]
-
-router = routers.SimpleRouter()
-router.register('breed', BreedViewSet)
+] + router.urls
