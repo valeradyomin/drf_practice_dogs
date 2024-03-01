3f56baa45016f8b49dec3be711fa3b62ba961d5a
Valera <wahrwolffe@gmail.com>
2024-03-01 03:12:45 +0300

    26.2. Celery django_celery_beat with send_mail_about_birthday

--- app_dogs/tasks.py
+++ app_dogs/tasks.py
@@ -1,6 +1,16 @@
+import datetime
+
 from celery import shared_task
+
+from app_dogs.models import Dog
 
 
 @shared_task
 def send_message_about_like(chat_id):
     print(f'сообщение о лайке отправлено в чат {chat_id}')
+
+
+def send_mail_about_birthday():
+    dog_list = Dog.objects.filter(birthday=datetime.date.today())
+    for dog in dog_list:
+        print(f'Send mail about {dog} birthday to {dog.owner.username}')
