1265227406ac091273ca4a6cb2e43427d70655c8
Valera Demin <wahrwolffe@gmail.com>
2024-03-02 01:37:52 +0300

    26.2. Celery add MyTelegramBot WTF?

--- users/services.py
+++ users/services.py
@@ -5,8 +5,10 @@ from config import settings
 
 class MyTelegramBot:
     URL = 'https://api.telegram.org/bot'
-    TOKEN = settings.TELEGRAM_BOT_TOKEN
-    CHAT_ID = settings.TELEGRAM_BOT_CHAT_ID
+    # TOKEN = settings.TELEGRAM_BOT_TOKEN
+    # CHAT_ID = settings.TELEGRAM_BOT_CHAT_ID
+    TOKEN = ''
+    CHAT_ID = ''
 
     def send_message(self, text):
         requests.post(
@@ -16,3 +18,13 @@ class MyTelegramBot:
                 'text': text
             },
         )
+
+
+bot = MyTelegramBot()
+cht_id = bot.CHAT_ID
+token = bot.TOKEN
+
+print(token)
+print(cht_id)
+
+bot.send_message(text='Hi, Sky!')
